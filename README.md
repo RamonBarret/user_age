065846989cd6e32c3b75676241b3d670fc2b344f
Ramon Barreto Medrado <105830248+RamonBarret@users.noreply.github.com>
2022-09-25 21:20:58 -0300

    version01

---
+++ README.md
@@ -0,0 +1,21 @@
+# Curso de JavaScript
+
+>Programa para verificar a idade de uma pessoa com base em sua data de nascimento.
+
+![preview](./.github/preview.png)
+
+Projeto desevolvido através dos conhenhecimentos obtidos no cursos lecionados pelo professor Gustavo Guanabara, Curso em Vídeo.
+Nesse programa a pessoa vai digitar uma data nascimento e selecionar um sexo, como resultado vai exibir na tela a idade compilada e uma imagem ilustrativa com base nos dados entrados.
+
+[🔗 Clique aqui para acessar](https://ramonbarret.github.io/user_age/)
+
+## 🛠 Tecnologias
+
+- HTML5
+- CSS3
+- JavaScript
+- Git e Github
+
+## 📲 Contato
+
+Email: ramon_barreto_medrado@hotmail.com
---
+++ script.js
@@ -0,0 +1,58 @@
+function verificar () {
+    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) >= ano) {
+        //Bom dia!
+      window.alert('[ERRO] - Verifique os dados e tente novamente!')
+    }
+    else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/criaca_homemp.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/jovem_homemp.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/homem_adultop.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/homem_idosop.jpg')
+            }
+
+
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/menina_criancapq.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/jovem_mulherp.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/mulher_adultap.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/idosa_mulherp.jpg')
+            }
+
+        }
+        res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
